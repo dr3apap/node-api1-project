2514340738a2cb1ba4559a45073f2849f837657a
Adebola  Adesina <dreapap@gmail.com>
2020-03-10 09:56:34 -0700

    Added a Get:id with a specific user

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const Joi = require("joi");
 const express = require("express");
 const shortid = require("shortid");
 
@@ -7,21 +8,39 @@ const server = express();
 server.use(express.json());
 
 let users = [];
-
+let sendUserError = (msg, res) => {
+  res.status(400);
+  res.json({ errorMessage: msg });
+  return;
+};
 server.post("/api/users", (req, res) => {
   const usersInfo = req.body;
 
   usersInfo.id = shortid.generate();
 
-  res.status(201).json(usersInfo);
+  //   res.status(201).json(usersInfo);
 
-  users.push(usersInfo);
+  // users.push(usersInfo);
+  if (usersInfo.hasOwnProperty("name" && "bio")) {
+    res.status(201).json(usersInfo);
+    users.push(usersInfo);
+  } else {
+    return sendUserError("Please provide name and bio for the user.", res);
+  }
 });
-
-server.listen(Port, () =>
-  console.log(`\n** Server listening on http://localhost:${Port}**\n`)
-);
 
 server.get("/api/users", (req, res) => {
   res.status(200).json(users);
 });
+
+server.get("/api/users:id", (req, res) => {
+  const user = users.find(user => user.id === parseInt(req.params.id));
+  if (!user)
+    res.status(404).send("The user with the specified ID does not exist.");
+  res.send(user);
+});
+const port = process.env.PORT || 5000;
+
+server.listen(Port, () =>
+  console.log(`\n** Server listening on http://localhost:${Port}**\n`)
+);
